530b30bc74bed61d26978f954e3f99d048fbc241
rusha271 <rushabhnakum440@gmail.com>
2025-01-30 18:52:33 +0530

    Removed build and dist files from tracking

--- dist/LogExtraction/_internal/Controller/__init__.py
+++
@@ -1,7 +0,0 @@
-#__all__ = ["fetch_logs_controller"]
-
-import os 
-import sys
-
-# Add the path to the Controller folder to the system path
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
